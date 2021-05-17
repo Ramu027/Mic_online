b6a369e64c13ee0d21d1a82921e3665cc8548429
Ramu027 <51402263+Ramu027@users.noreply.github.com>
2021-05-17 10:39:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+def A(a):
+    print((a%3*50) + (int(a/3)*130))
+def B(b):
+    print((b%2*30) + (int(b/2)*45))
+def C(c):
+    print((c%5*20) + (int(c/5)*80))
+def D(d):
+    print(d*15)
+a = int(input('enter A items:'))
+A(a)
+b =int(input('enter B items:'))
+B(b)
+c = int(input('enter C items:'))
+C(c)
+d = int(input('enter d items:'))
+D(c)
